7c4f11c6c2425e24ae4c299627035b35002fc87c
monill <juaorok@hotmail.com>
2025-03-26 17:29:03 -0300

    Update PaymentTypesTableSeeder.php

--- database/seeders/PaymentTypesTableSeeder.php
+++ database/seeders/PaymentTypesTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,14 +14,15 @@ class PaymentTypesTableSeeder extends Seeder
      */
     public function run(): void
     {
+        $now = Carbon::now();
         DB::table('payment_types')->insert([
-            ['name' => 'Cartão de Crédito', 'code' => 'credit_card', 'is_enabled' => true],
-            ['name' => 'Cartão de Débito', 'code' => 'debit_card', 'is_enabled' => true],
-            ['name' => 'Pix', 'code' => 'pix', 'is_enabled' => true],
-            ['name' => 'Boleto Bancário', 'code' => 'boleto', 'is_enabled' => true],
-            ['name' => 'Dinheiro', 'code' => 'cash', 'is_enabled' => true],
-            ['name' => 'Cheque', 'code' => 'cheque', 'is_enabled' => true],
-            ['name' => 'Transferência Bancária', 'code' => 'bank_transfer', 'is_enabled' => true],
+            ['name' => 'Cartão de Crédito', 'code' => 'credit_card', 'is_enabled' => true, 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Cartão de Débito', 'code' => 'debit_card', 'is_enabled' => true, 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Pix', 'code' => 'pix', 'is_enabled' => true, 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Boleto Bancário', 'code' => 'boleto', 'is_enabled' => true, 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Dinheiro', 'code' => 'cash', 'is_enabled' => true, 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Cheque', 'code' => 'cheque', 'is_enabled' => true, 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Transferência Bancária', 'code' => 'bank_transfer', 'is_enabled' => true, 'created_at' => $now, 'updated_at' => $now],
         ]);
     }
 }
