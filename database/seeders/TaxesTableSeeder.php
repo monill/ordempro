764a94b423c3516bce9a37817f109d9e7f75a16d
monill <juaorok@hotmail.com>
2025-03-26 17:29:11 -0300

    Update TaxesTableSeeder.php

--- database/seeders/TaxesTableSeeder.php
+++ database/seeders/TaxesTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,11 +14,13 @@ class TaxesTableSeeder extends Seeder
      */
     public function run(): void
     {
+        $now = Carbon::now();
         DB::table('taxes')->insert([
-            ['name' => 'VAT', 'rate' => 20.00, 'is_inclusive' => true, 'is_enabled' => true],
-            ['name' => 'ICMS', 'rate' => 18.00, 'is_inclusive' => false, 'is_enabled' => true],
-            ['name' => 'ISS', 'rate' => 5.00, 'is_inclusive' => false, 'is_enabled' => true],
-            ['name' => 'GST', 'rate' => 10.00, 'is_inclusive' => true, 'is_enabled' => true],
+            ['name' => 'Nenhum', 'rate' => 0, 'is_inclusive' => false, 'is_enabled' => true,  'created_at' => $now, 'updated_at' => $now,],
+            ['name' => 'VAT', 'rate' => 20.00, 'is_inclusive' => true, 'is_enabled' => true,  'created_at' => $now, 'updated_at' => $now,],
+            ['name' => 'ICMS', 'rate' => 18.00, 'is_inclusive' => false, 'is_enabled' => true,  'created_at' => $now, 'updated_at' => $now,],
+            ['name' => 'ISS', 'rate' => 5.00, 'is_inclusive' => false, 'is_enabled' => true,  'created_at' => $now, 'updated_at' => $now,],
+            ['name' => 'GST', 'rate' => 10.00, 'is_inclusive' => true, 'is_enabled' => true,  'created_at' => $now, 'updated_at' => $now,],
         ]);
     }
 }
