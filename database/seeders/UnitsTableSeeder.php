2cf788352fbf471953df8c0d6bb3087da6ba99e2
monill <juaorok@hotmail.com>
2025-03-26 17:29:16 -0300

    Update UnitsTableSeeder.php

--- database/seeders/UnitsTableSeeder.php
+++ database/seeders/UnitsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,22 +14,23 @@ class UnitsTableSeeder extends Seeder
      */
     public function run(): void
     {
+        $now = Carbon::now();
         DB::table('units')->insert([
-            ['name' => 'None', 'short_code' => 'None'],
-            ['name' => 'Box', 'short_code' => 'Box'],
-            ['name' => 'Pieces', 'short_code' => 'Pcs'],
-            ['name' => 'Bag', 'short_code' => 'Bag'],
-            ['name' => 'Bottle', 'short_code' => 'Btl'],
-            ['name' => 'Kilogram', 'short_code' => 'Kg'],
-            ['name' => 'Gram', 'short_code' => 'g'],
-            ['name' => 'Liter', 'short_code' => 'L'],
-            ['name' => 'Meter', 'short_code' => 'm'],
-            ['name' => 'Centimeter', 'short_code' => 'cm'],
-            ['name' => 'Millimeter', 'short_code' => 'mm'],
-            ['name' => 'Pack', 'short_code' => 'Pk'],
-            ['name' => 'Dozen', 'short_code' => 'Dz'],
-            ['name' => 'Ton', 'short_code' => 'T'],
-            ['name' => 'Pair', 'short_code' => 'Pr']
+            ['name' => 'None', 'short_code' => 'None', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Box', 'short_code' => 'Box', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Pieces', 'short_code' => 'Pcs', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Bag', 'short_code' => 'Bag', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Bottle', 'short_code' => 'Btl', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Kilogram', 'short_code' => 'Kg', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Gram', 'short_code' => 'g', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Liter', 'short_code' => 'L', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Meter', 'short_code' => 'm', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Centimeter', 'short_code' => 'cm', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Millimeter', 'short_code' => 'mm', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Pack', 'short_code' => 'Pk', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Dozen', 'short_code' => 'Dz', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Ton', 'short_code' => 'T', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Pair', 'short_code' => 'Pr', 'created_at' => $now, 'updated_at' => $now]
         ]);
     }
 }
