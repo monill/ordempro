50bd2994fffcfb3af25e297a7aff14e7ff17ef51
monill <juaorok@hotmail.com>
2025-03-26 17:29:09 -0300

    Create SettingsTableSeeder.php

---
+++ database/seeders/SettingsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SettingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $now = Carbon::now();
+        DB::table('settings')->insert([
+            'name' => 'app_name', 'value' => 'OrdemPro', 'created_at' => $now, 'updated_at' => $now
+        ]);
+    }
+}
