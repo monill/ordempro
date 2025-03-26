fa1c61f4e7d5fd77566d9e17a258ae0ed83202fe
monill <juaorok@hotmail.com>
2025-03-26 17:28:44 -0300

    Update CountriesTableSeeder.php

--- database/seeders/CountriesTableSeeder.php
+++ database/seeders/CountriesTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,12 +14,13 @@ class CountriesTableSeeder extends Seeder
      */
     public function run(): void
     {
+        $now = Carbon::now();
         DB::table('countries')->insert([
-            ['code' => 'USA', 'name' => 'United States', 'phone_code' => '+1', 'currency_code' => 'USD'],
-            ['code' => 'BRA', 'name' => 'Brazil', 'phone_code' => '+55', 'currency_code' => 'BRL'],
-            ['code' => 'GBR', 'name' => 'United Kingdom', 'phone_code' => '+44', 'currency_code' => 'GBP'],
-            ['code' => 'CAN', 'name' => 'Canada', 'phone_code' => '+1', 'currency_code' => 'CAD'],
-            ['code' => 'FRA', 'name' => 'France', 'phone_code' => '+33', 'currency_code' => 'EUR'],
+            ['code' => 'USA', 'name' => 'United States', 'phone_code' => '+1', 'currency_code' => 'USD', 'created_at' => $now, 'updated_at' => $now],
+            ['code' => 'BRA', 'name' => 'Brazil', 'phone_code' => '+55', 'currency_code' => 'BRL', 'created_at' => $now, 'updated_at' => $now],
+            ['code' => 'GBR', 'name' => 'United Kingdom', 'phone_code' => '+44', 'currency_code' => 'GBP', 'created_at' => $now, 'updated_at' => $now],
+            ['code' => 'CAN', 'name' => 'Canada', 'phone_code' => '+1', 'currency_code' => 'CAD', 'created_at' => $now, 'updated_at' => $now],
+            ['code' => 'FRA', 'name' => 'France', 'phone_code' => '+33', 'currency_code' => 'EUR', 'created_at' => $now, 'updated_at' => $now],
         ]);
     }
 }
