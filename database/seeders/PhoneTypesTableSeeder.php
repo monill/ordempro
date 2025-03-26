e5a834960e6b926209efebc0943734c8ebfd6e5d
monill <juaorok@hotmail.com>
2025-03-26 17:29:05 -0300

    Update PhoneTypesTableSeeder.php

--- database/seeders/PhoneTypesTableSeeder.php
+++ database/seeders/PhoneTypesTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,12 +14,13 @@ class PhoneTypesTableSeeder extends Seeder
      */
     public function run(): void
     {
+        $now = Carbon::now();
         DB::table('phone_types')->insert([
-            ['name' => 'Celular'],
-            ['name' => 'Comercial'],
-            ['name' => 'Residencial'],
-            ['name' => 'WhatsApp'],
-            ['name' => 'Suporte'],
+            ['name' => 'Celular', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Comercial', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Residencial', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'WhatsApp', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Suporte', 'created_at' => $now, 'updated_at' => $now],
         ]);
     }
 }
