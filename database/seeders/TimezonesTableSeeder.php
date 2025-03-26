ca43b7bf20861cde7dab83f6308d162565c549e2
monill <juaorok@hotmail.com>
2025-03-26 17:29:14 -0300

    Update TimezonesTableSeeder.php

--- database/seeders/TimezonesTableSeeder.php
+++ database/seeders/TimezonesTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,12 +14,13 @@ class TimezonesTableSeeder extends Seeder
      */
     public function run(): void
     {
+        $now = Carbon::now();
         DB::table('timezones')->insert([
-            ['name' => 'UTC', 'offset' => '+00:00'],
-            ['name' => 'America/New_York', 'offset' => '-05:00'],
-            ['name' => 'America/Sao_Paulo', 'offset' => '-03:00'],
-            ['name' => 'Europe/London', 'offset' => '+00:00'],
-            ['name' => 'Asia/Tokyo', 'offset' => '+09:00'],
+            ['name' => 'UTC', 'offset' => '+00:00', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'America/New_York', 'offset' => '-05:00', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'America/Sao_Paulo', 'offset' => '-03:00', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Europe/London', 'offset' => '+00:00', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Asia/Tokyo', 'offset' => '+09:00', 'created_at' => $now, 'updated_at' => $now],
         ]);
     }
 }
