bba3dd242f155b49cad2c22e9a6dfc1487381a7d
monill <juaorok@hotmail.com>
2025-03-26 17:28:32 -0300

    Update AddressTypesTableSeeder.php

--- database/seeders/AddressTypesTableSeeder.php
+++ database/seeders/AddressTypesTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,14 +14,14 @@ class AddressTypesTableSeeder extends Seeder
      */
     public function run(): void
     {
+        $now = Carbon::now();
         DB::table('address_types')->insert([
-            ['name' => 'Residencial'],
-            ['name' => 'Faturamento'],
-            ['name' => 'Entrega'],
-            ['name' => 'Correspondência'],
-            ['name' => 'Cobrança'],
-            ['name' => 'Residencial'],
-            ['name' => 'Comercial'],
+            ['name' => 'Residencial', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Comercial', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Faturamento', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Entrega', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Correspondência', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Cobrança', 'created_at' => $now, 'updated_at' => $now],
         ]);
     }
 }
